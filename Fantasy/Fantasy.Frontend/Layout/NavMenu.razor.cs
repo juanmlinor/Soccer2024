615b230f3ed30307a21cb9d6ba93678d39a83077
JuanLino <jumalire55@outlook.com>
2024-11-06 16:04:59 -0800

    Controller Countries

--- Fantasy/Fantasy.Frontend/Layout/NavMenu.razor.cs
+++ Fantasy/Fantasy.Frontend/Layout/NavMenu.razor.cs
@@ -1,8 +1,13 @@
-﻿namespace Fantasy.Frontend.Layout
+﻿using Fantasy.Shared.Resources;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Fantasy.Frontend.Layout
 {
     public partial class NavMenu
     {
         private bool collapseNavMenu = true;
+        [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
 
         private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
 
--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Fantasy.Frontend;
 using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,5 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7062") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddLocalization();
+builder.Services.AddSweetAlert2();
+
 
 await builder.Build().RunAsync();
